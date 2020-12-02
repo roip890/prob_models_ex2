bd5ebe9a8754bf86f6d8623878a6dc7361f88d30
Roi Peretz <roip890@gmail.com>
2020-12-02 19:30:36 +0200

    init

---
+++ ex2.py
@@ -0,0 +1,13 @@
+import sys
+
+
+
+
+
+
+
+if len(sys.argv) >= 4:
+    development_set_filename = sys.argv[1]
+    test_set_filename = sys.argv[2]
+    input_word = sys.argv[3]
+    output_filename = sys.argv[4]
